b9a3637aeb52c0277e40d6493b8b987500ecdd80
FadiaSami <87935994+FadiaSami@users.noreply.github.com>
2024-01-24 19:00:01 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -4,11 +4,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index</title>
+    <title>Document</title>
 </head>
 <body>
     <?php
-    echo "PHP";
+    session_start();
+    
+    $username = $password = "";
+    $password_err=$username_err= $error_login = "";
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "tickitingsystem";
+
+    $conn= mysqli_connect($servername,$username,$password,$db_name) or die ("Connection failed");
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        // Check if username is empty
+        if(empty($_POST["username"])){
+            $username_err = "Please enter username.";
+        } else{
+            $username = $_POST["username"];
+        }
+        
+        // Check if password is empty
+        if(empty($_POST["password"])){
+            $password_err = "Please enter your password.";
+        } else{
+            $password = $_POST["password"];
+        }
+
+        if(empty($username_err) && empty($password_err)){
+            $sql = "Select * from employee";
+            $result = mysqli_query($conn,$sql);
+
+            if (mysqli_num_rows ($result) > 0){
+                
+                while($row = mysqli_fetch_assoc($result)){
+                    if ($row["ID"] == $username && $row ["password"] == $password)
+                    {
+                        $_SESSION["Name"] = $row['name'];
+                        $_SESSION["ID"] = $row ['ID'];
+                        header("location: Employee.php");
+                        break;
+                    }
+                    else
+                    $error_login = "Error username or passowrd";
+                }
+            }
+        }
+    }
     ?>
     
     <form action="Login.php" method = "post">
